e6a3cd7fa5bd5b92eb94e2f8f749e1ca50bbbc02
hokin2023 <hokin990@gmail.com>
2023-05-25 16:16:05 -0700

    added homeroutes and index

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const dashboardRoutes = require('./dashboard');
+const dashboardRoutes = require('./Dashboard');
 
 router.use('/user', userRoutes);
 router.use('/dashboard', dashboardRoutes);
 
+module.exports = router;    
