720b033f9d8472754a423d3a51144c8182377558
hokin2023 <hokin990@gmail.com>
2023-05-27 16:57:32 -0700

    added post model

--- Develop/models/Post.js
+++ Develop/models/Post.js
@@ -1,10 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { underscoredIf } = require('sequelize/types/utils');
 
-class Comment extends Model {}
 
-Comment.init(
+class Post extends Model {}
+
+Post.init(
     {
     id: {
         type: DataTypes.INTEGER,
@@ -12,27 +12,21 @@ Comment.init(
         allowNull: false,
         autoIncrement: true
     },
-    user_id: {
-        type: DataTypes.INTEGER,
+    title: {
+        type: DataTypes.STRING,
         allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+        
     },
-post_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-        model: 'post',
-        key: 'id'
-    }
+post_content: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+    
 },
-comment_text: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-        len: [1]
+user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+    model: 'user',
+    key: 'id'
     }
 }
 
@@ -41,8 +35,8 @@ comment_text: {
     sequelize,
 freezeTableName: true,
 underscored: true,
-modelName: 'comment'
+modelName: 'post'
 }
 );
 
-module.exports = Comment;
+module.exports = Post;
